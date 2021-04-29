5f13a2a7938c9a8071cd1074b109ace8331d322c
Prokop <prokop.schield@protonmail.com>
2021-04-30 00:01:27 +0200

    Major improvements

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,42 @@ import NodeSiteClient, { NodeSiteRequest } from 'nodesite.eu';
 
 const memes = getConfig('memes');
 
+let memecount = -1;
+const oldmemes = [ ...memes.array ];
+const newmemes: string[] = [];
+
+for (const meme of oldmemes) {
+	if ((typeof meme === 'string') && (meme.length === 64) && (!newmemes.includes(meme))) {
+		memes.__set((++memecount).toString(), meme);
+		newmemes.push(meme);
+	}
+}
+
 NodeSiteClient.create('memestream', '/', () => ({ statusCode: 302, head: { Location: '/src/index.html' }}), '.');
 
 NodeSiteClient.create('memestream', '/post', (request: NodeSiteRequest) => {
 	const meme = request.body?.toString?.();
-	if (meme && (meme.length === 64)) {
-		if (!memes.array.includes(meme)) {
-			memes.array.push(meme);
-			return ({
-				statusCode: 302,
-				head: {
-					Location: '/last',
-				}
-			});
-		}
-	} return ({
-		body: '{"error": "Old meme!"}',
+	if (meme && (meme.length === 64) && !memes.array.includes(meme)) {
+		memes.__set((++memecount).toString(), meme);
+	}
+	return ({
+		body: memecount,
 	});
 });
 
 NodeSiteClient.create('memestream', '/last', () => {
 	return ({
-		body: `${memes.array.length}`,
+		body: `${memecount}`,
 	});
 });
 
 NodeSiteClient.create('memestream', '/get', (request: NodeSiteRequest) => {
+	let memeid = request.uri.replace(/[^0-9]+/g, '') || '0';
 	return ({
-		body: `"${memes.__getString(request.uri.replace(/[^0-9]+/g, ''))}"`,
+		body: (
+			memes.__has(memeid)
+			? `"${memes.__getString(memeid)}"`
+			: `"${memes.__getString(Math.floor(Math.random() * memecount).toString())}"`
+		)
 	});
 });
