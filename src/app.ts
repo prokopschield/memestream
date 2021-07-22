96af98204662a23a7ee968db77dc25445300a67c
Prokop <prokop.schield@protonmail.com>
2021-07-22 12:19:01 +0200

    Run prettier

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 let waiting = 0;
 
-function givememe () {
+function givememe() {
 	const input = document.createElement('input');
 	input.setAttribute('type', 'file');
 	input.onchange = (e: Event) => {
@@ -10,27 +10,34 @@ function givememe () {
 			fetch('https://memestream.nodesite.eu/static/put', {
 				method: 'PUT',
 				body: file,
-			}).then(res => res.text()).then(a => fetch('/post', {
-				method: 'POST',
-				body: a,
-			})).then(response => response.json()).then(res => {
-				--waiting;
-				if (res.error) {
-					if (waiting == 0) {
-						alert(res.error);
-						input.click();
+			})
+				.then((res) => res.text())
+				.then((a) =>
+					fetch('/post', {
+						method: 'POST',
+						body: a,
+					})
+				)
+				.then((response) => response.json())
+				.then((res) => {
+					--waiting;
+					if (res.error) {
+						if (waiting == 0) {
+							alert(res.error);
+							input.click();
+						}
+					} else {
+						showmemes(+res);
 					}
-				} else {
-					showmemes(+res);
-				}
-			}).catch(console.log);
+				})
+				.catch(console.log);
 		}
 	};
 	input.click();
 }
 
-function showmemes (res: number) {
-	function resizememes () {
+function showmemes(res: number) {
+	function resizememes() {
 		for (const img of document.querySelectorAll('img')) {
 			img.style.width = `${innerWidth - 28}px`;
 		}
@@ -41,7 +48,7 @@ function showmemes (res: number) {
 	function givememe() {
 		if (!memenow) return;
 		fetch(`/get/${--memenow}`)
-			.then(res => res.json())
+			.then((res) => res.json())
 			.then((id: string) => {
 				const img = document.createElement('img');
 				if (!img || !id) return givememe();
@@ -49,15 +56,15 @@ function showmemes (res: number) {
 				img.onload = img.onerror = () => {
 					resizememes();
 					testmeme();
-				}
+				};
 				document.querySelector('#memes')!.appendChild(img);
 			})
 			.catch(console.log);
 	}
 
-	function testmeme () {
-		const img = [ ...document.querySelectorAll('img') ].pop();
-		if (!img || (img.offsetTop < (window.innerHeight + window.pageYOffset))) {
+	function testmeme() {
+		const img = [...document.querySelectorAll('img')].pop();
+		if (!img || img.offsetTop < window.innerHeight + window.pageYOffset) {
 			givememe();
 		}
 	}
--- src/index.ts
+++ src/index.ts
@@ -8,41 +8,50 @@ const domain = config.__getString('domain');
 const memes = getConfig('memes').__getField('memes');
 
 let memecount = -1;
-const oldmemes = [ ...memes.array ];
+const oldmemes = [...memes.array];
 const newmemes: string[] = [];
 
 for (const meme of oldmemes) {
-	if ((typeof meme === 'string') && (meme.length === 64) && (!newmemes.includes(meme))) {
+	if (
+		typeof meme === 'string' &&
+		meme.length === 64 &&
+		!newmemes.includes(meme)
+	) {
 		memes.__set((++memecount).toString(), meme);
 		newmemes.push(meme);
 	}
 }
 
-create(domain, '/', () => ({ statusCode: 302, head: { Location: '/src/index.html' }}), '.');
+create(
+	domain,
+	'/',
+	() => ({ statusCode: 302, head: { Location: '/src/index.html' } }),
+	'.'
+);
 
 create(domain, '/post', (request: NodeSiteRequest) => {
 	const meme = request.body?.toString?.();
-	if (meme && (meme.length === 64) && !memes.array.includes(meme)) {
+	if (meme && meme.length === 64 && !memes.array.includes(meme)) {
 		memes.__set((++memecount).toString(), meme);
 	}
-	return ({
+	return {
 		body: `${memecount}`,
-	});
+	};
 });
 
 create(domain, '/last', () => {
-	return ({
+	return {
 		body: `${memecount}`,
-	});
+	};
 });
 
 create(domain, '/get', (request: NodeSiteRequest) => {
 	let memeid = request.uri.replace(/[^0-9]+/g, '') || '0';
-	return ({
-		body: (
-			memes.__has(memeid)
+	return {
+		body: memes.__has(memeid)
 			? `"${memes.__getString(memeid)}"`
-			: `"${memes.__getString(Math.floor(Math.random() * memecount).toString())}"`
-		)
-	});
+			: `"${memes.__getString(
+					Math.floor(Math.random() * memecount).toString()
+			  )}"`,
+	};
 });
